a3274e0e6fe416ef06384be06e53c5c90dc37a68
Mattchine <github.04shk@simplelogin.co>
2023-05-09 18:51:10 +0700

    add starship switch kitty to wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.font = wezterm.font 'Fira Code Nerd Font'
+config.font_size = 18.0
+
+
+-- maximize on start up
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- and finally, return the configuration to wezterm
+return config
