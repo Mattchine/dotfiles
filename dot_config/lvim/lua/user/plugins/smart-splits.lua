cc7f8172a664230c06e8d7daa45757cd201cd00a
Mattchine <github.04shk@simplelogin.co>
2023-05-22 22:59:49 +0700

    feat: add vertical resize in wezterm and lvim via smart-splits plugin

--- dot_config/lvim/lua/user/plugins/smart-splits.lua
+++ dot_config/lvim/lua/user/plugins/smart-splits.lua
@@ -10,10 +10,10 @@ M.config = function()
         -- resizing splits
         -- these keymaps will also accept a range,
         -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-        -- vim.api.nvim_set_keymap('n', '<A-h>', ":SmartResizeLeft<cr>", { silent = true })
+        vim.api.nvim_set_keymap('n', '<A-h>', ":SmartResizeLeft<cr>", { silent = true })
         -- vim.api.nvim_set_keymap('n', '<A-j>', ":SmartResizeDown<cr>", { silent = true })
         -- vim.api.nvim_set_keymap('n', '<A-k>', ":SmartResizeUp<cr>", { silent = true })
-        -- vim.api.nvim_set_keymap('n', '<A-l>', ":SmartResizeRight<cr>", { silent = true })
+        vim.api.nvim_set_keymap('n', '<A-l>', ":SmartResizeRight<cr>", { silent = true })
         -- moving between splits
         vim.api.nvim_set_keymap('n', '<C-h>', ":SmartCursorMoveLeft<cr>", { silent = true })
         vim.api.nvim_set_keymap('n', '<C-j>', ":SmartCursorMoveDown<cr>", { silent = true })
